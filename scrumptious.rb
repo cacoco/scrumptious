87365fdc671ba89587279497a0999734f1e59603
Christopher Coco <coco@heroku.com>
2011-10-16 15:41:10 -0700

    Updated to use variables more.

--- scrumptious.rb
+++ scrumptious.rb
@@ -7,6 +7,7 @@ class Scrumptious < Sinatra::Base
 
   set :campfire_domain => ENV["CAMPFIRE_DOMAIN"]
   set :campfire_token => ENV["CAMPFIRE_TOKEN"]
+  set :campfire_room => ENV["CAMPFIRE_ROOM"]
 
   set :scrumy_project => ENV["SCRUMY_PROJECT"]
   set :scrumy_password => ENV["SCRUMY_PASSWORD"]
@@ -16,14 +17,15 @@ class Scrumptious < Sinatra::Base
   end
 
   post "/" do
-    handle_action(params[:action])
+    handle(params[:action])
   end
 
   helpers do
-    def handle_action(action)
-      room = VaultRoom.new(settings.campfire_domain, settings.campfire_token)
+    def handle(action)
+      room = Campfire::Room.new(settings.campfire_domain,
+                          settings.campfire_token,
+                          settings.campfire_room)
 
-      time = params[:time]
       resource = params[:resource]
       data = params[:data]
       id = params[:id]
@@ -36,24 +38,24 @@ class Scrumptious < Sinatra::Base
       elsif 'destroy' == action
         logger.info action
       end
-      logger.info time
-      logger.info resource
-      logger.info id
-      logger.info data
 
       unless data == 'test'
-        json = JSON.parse(data)
+        json = JSON.parse data
+
         unless "order_tasks" == action  # don't care about this action
-          scrumy = Scrumy.new(settings.scrumy_project, settings.scrumy_password)
-          detail = scrumy.get_task_info(id)
-          message = "#{resource.capitalize} [#{detail["title"]}], assigned to: [#{detail["scrumer"]["name"]}] was #{Inflectionist.past_tensed(action)}. "
-          json.each do |key, value|
-            if value.kind_of?(Array)
-              message << "#{key.capitalize} changed: [#{value[0]} => #{value[1]}]"
-            else
-              message << "#{key.capitalize} changed: [#{value}]"
-            end
+          key = json.keys.first
+          value = json[key]
+
+          if value.kind_of?(Array)
+            change = "#{key.capitalize} changed: [#{value[0]} => #{value[1]}]"
+          else
+            change = "#{key.capitalize} changed: [#{value}]"
           end
+
+          scrumy = Scrumy::Client.new(settings.scrumy_project, settings.scrumy_password)
+          detail = scrumy.get_info(id, resource)
+          message = "#{resource.capitalize} [#{detail["title"]}] assigned to: [#{detail["scrumer"]["name"]}] was #{Inflectionist.past_tensed(action)}. #{change}"
+
           logger.info message
           room.send_message message
         end
@@ -62,53 +64,58 @@ class Scrumptious < Sinatra::Base
   end
 end
 
-class VaultRoom
-  # Scrumy campfire user, TODO: move config outside of application
-  def initialize(domain, token)
-    @campfire = Tinder::Campfire.new domain, :token => token, :ssl => true
-    @room = @campfire.find_room_by_id(412680)
-    @room.join
-  end
+module Campfire
+  class Room
+    def initialize(domain, token, room)
+      @campfire = Tinder::Campfire.new domain, :token => token, :ssl => true
+      @room = @campfire.find_room_by_id(room)
+      @room.join
+    end
 
-  def send_message(message, options = {})
-    @room.speak message
-  end
+    def send_message(message, options = {})
+      @room.speak message
+    end
 
-  def send_paste(message, options = {})
-    @room.paste message
+    def send_paste(message, options = {})
+      @room.paste message
+    end
   end
 end
 
-class Scrumy
-  SCRUMY_API_URL = 'https://scrumy.com/api'
+module Scrumy
+  class Client
+    SCRUMY_API_URL = 'https://scrumy.com/api'
+    FORMAT = 'json'
 
-  def initialize(project, password)
-    @project, @password = project, password
-  end
+    attr_reader :url
 
-  def get_task_info(id)
-    self.get("https://scrumy.com/api/tasks/#{id}.json", "task")
-  end
+    def initialize(project, password)
+      @project, @password = project, password
+    end
 
-  protected
-      # `#get` provides the nuts and bolts for retrieving resources.  Give it a
-      # resource URL and a root key and it will return either an array of hashes
-      # at that root key or a single hash with values found at that key.
-      #
-      # For example if the resource returns `{"foo"=>{"id"=>1, "bar"=>"baz"}}`
-      # then `#get(some_url, "foo")` will return the value of `"foo"` from the hash:
-      # `{"id"=>1, "bar"=>"baz"}`.  This is important because later on in the models
-      # we assign all the values in the latter hash as instance variables on the
-      # model objects.
-      def get(url, root)
-        begin
-          # Start by creating a new `RestClient::Resource` authenticated with
-          # the `@project` name and `@password`.
-          resource = RestClient::Resource.new url, {:user => @project, :password => @password}
+    def get_info(id, resource)
+      self.get("#{SCRUMY_API_URL}/#{resource.pluralize}/#{id}.#{FORMAT}", resource)
+    end
 
-          # `GET` the resource
-          resource.get {|response, request, result, &block|
-            case response.code
+    protected
+    # `#get` provides the nuts and bolts for retrieving resources.  Give it a
+    # resource URL and a root key and it will return either an array of hashes
+    # at that root key or a single hash with values found at that key.
+    #
+    # For example if the resource returns `{"foo"=>{"id"=>1, "bar"=>"baz"}}`
+    # then `#get(some_url, "foo")` will return the value of `"foo"` from the hash:
+    # `{"id"=>1, "bar"=>"baz"}`.  This is important because later on in the models
+    # we assign all the values in the latter hash as instance variables on the
+    # model objects.
+    def get(url, root)
+      begin
+        # Start by creating a new `RestClient::Resource` authenticated with
+        # the `@project` name and `@password`.
+        resource = RestClient::Resource.new url, {:user => @project, :password => @password}
+
+        # `GET` the resource
+        resource.get {|response, request, result, &block|
+          case response.code
             when 200
               # and on success parse the response
               json = JSON.parse(response.body)
@@ -123,11 +130,12 @@ class Scrumy
               end
             else
               response.return!(request, result, &block)
-            end
-          }
-        rescue => e
-          # Rescue and reraise with the current `@url` for debugging purposes
-          raise "Problem fetching #{@url} because #{e.message}"
-        end
+          end
+        }
+      rescue => e
+        # Rescue and reraise with the current `@url` for debugging purposes
+        raise "Problem fetching #{@url} because #{e.message}"
       end
+    end
+  end
 end
